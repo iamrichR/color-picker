b85fdea336f1ef67cbd8c8c86ea8d2add8647859
iamrichR <44210678+iamrichR@users.noreply.github.com>
2020-05-02 16:18:19 -0400

    dynamic slider backgrounds working

--- hsla_prototype/script.js
+++ hsla_prototype/script.js
@@ -17,11 +17,13 @@ window.onload = function(){
     hueSlider.onchange = function(){
         hue = hueSlider.value;
         updateSatBg();
+        updateLightBg();
         console.log(`hue: ${hue}`);
     };
     
     satSlider.onchange = function(){
         sat = satSlider.value;
+        updateLightBg();
         console.log(`sat: ${sat}`);
     };
 
@@ -33,9 +35,38 @@ window.onload = function(){
 
 
     updateSatBg();
+    updateLightBg();
 }
 
 function updateSatBg(){
-    console.log(`linear-gradient(to right, hsla(${hue},0%,${light}%,1.0), hsla(${hue},100%,${light}%,1.0));`);
-    document.getElementById("sat-slider-bg").style.background = `linear-gradient(to right, hsla(${hue},0%,${light}%,1.0), hsla(${hue},100%,${light}%,1.0));`;
+    let lowSatStr = `hsla(${hue},0%,${light}%,1.0)`;
+    let highSatStr = `hsla(${hue},100%,${light}%,1.0)`;
+
+    let canvas = document.getElementById('sat-slider-bg');
+    let colorStops = [lowSatStr, highSatStr];
+    
+    applyGradient(colorStops, canvas);
+}
+
+function updateLightBg(){
+    let lowLightStr = `hsla(${hue},${sat}%,0%,1.0)`;
+    let midLightStr = `hsla(${hue},${sat}%,50%,1.0)`;
+    let highLightStr = `hsla(${hue},${sat}%,100%,1.0)`;
+
+    let canvas = document.getElementById('light-slider-bg');
+    let colorStops = [lowLightStr, midLightStr, highLightStr];
+    
+    applyGradient(colorStops, canvas);
+}
+
+function applyGradient(colorStops, canvas){
+    let ctx = canvas.getContext('2d');
+    let gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    colorStops.forEach((colorStop, index) => {
+        //scale indexes to fit on a {0.0 -> 1.0} scale
+        let scaledIndex = index/(colorStops.length - 1);
+        gradient.addColorStop(scaledIndex, colorStop);
+    });
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }

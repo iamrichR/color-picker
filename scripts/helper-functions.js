efef4322e2046ffc0dfb76495e432942895ba599
iamrichR <44210678+iamrichR@users.noreply.github.com>
2020-05-15 13:17:22 -0400

    dynamic color code display complete

--- scripts/helper-functions.js
+++ scripts/helper-functions.js
@@ -71,6 +71,7 @@ function sliderOnInput(){
 function updateSampleBg(){
     let newColor = `hsla(${sliders.hue.value}, ${sliders.sat.value}%, ${sliders.light.value}%, ${sliders.alpha.value})`;
     document.getElementById('color-sample').style.backgroundColor = newColor;
+    updateColorCodes(newColor);
 }
 
 function getNewSliderBg(key){
@@ -97,4 +98,58 @@ function updateSliderBg(key){
     gradStr += ")";
 
     sliders[key].bg.style.background = gradStr;
+}
+
+function updateColorCodes(hsla_code){
+    document.getElementById('hsla-code').innerHTML = hsla_code;
+    let rgba_code = parseRGB(document.getElementById('color-sample').style.backgroundColor);
+    document.getElementById('rgba-code').innerHTML = rgba_code;
+    let hex_code = rgba_to_hex(rgba_code);
+    document.getElementById('hex-code').innerHTML = hex_code;
+}
+
+function parseRGB(code){
+    //if rgb, convert to rgba
+    let code_type = code.slice(0,4).toLowerCase();
+    if(code_type == 'rgba'){
+        return code;
+    } else if(code_type == 'rgb('){
+        return code.replace('rgb', 'rgba').replace(')',', 1.0)');
+    } else{
+        console.log("Error in ParseRBG - Generated code not in RGB/RGBA format");
+        console.log(`function input:  "${code}"`);
+        return -1;
+    }
+}
+
+function rgba_to_hex(rgba_code){
+    let hex_code = "#";
+    
+    let startIdx = rgba_code.indexOf('(')+1;
+    let endIdx = rgba_code.indexOf(',');
+    let red = parseInt(rgba_code.slice(startIdx, endIdx)).toString(16);
+    if(red.length == 1){ red += '0'};
+    
+    startIdx = endIdx+1
+    endIdx = rgba_code.indexOf(',', startIdx);
+
+    let green = parseInt(rgba_code.slice(startIdx, endIdx)).toString(16);
+    if(green.length == 1){ green += '0'};
+
+    startIdx = endIdx+1
+    endIdx = rgba_code.indexOf(',', startIdx);
+
+    let blue = parseInt(rgba_code.slice(startIdx, endIdx)).toString(16);
+    if(blue.length == 1){ blue += '0'};
+
+    startIdx = endIdx+1
+    endIdx = rgba_code.indexOf(')');
+
+    let alpha = parseInt(parseFloat(rgba_code.slice(startIdx, endIdx))*255).toString(16);
+    if(alpha.length == 1){ alpha += '0'};
+    if(alpha.toLowerCase() == 'ff'){ alpha = ''};
+
+    hex_code = `#${red}${green}${blue}${alpha}`;
+
+    return hex_code
 }
